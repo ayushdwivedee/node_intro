fdcaba25d399a4b0e0cf23fb029e944fffeb86c4
ayushdwivedee <ayushbhartiya1494927@gmail.com>
2024-12-17 21:59:42 +0530

    this 6th question is  completed

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const add=require("./sum")
 const mul=require("./mul")
+const sub=require("./subtraction")
+const div=require("./division")
+
 
 let sumA=Number(process.argv[3])
 let sumB=Number(process.argv[4])
+
+
 console.log(add(sumA,sumB))
+console.log(mul(sumA,sumB))
+console.log(sub(sumA,sumB))
+console.log(div(sumA,sumB))
